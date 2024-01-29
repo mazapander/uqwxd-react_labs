9fda1875decad4d0d79c9d4fcac2270a1ab30a58
mazapander <98283345+mazapander@users.noreply.github.com>
2024-01-29 14:16:46 +0100

    Update index.js

--- react-redux-master/src/reducers/index.js
+++ react-redux-master/src/reducers/index.js
@@ -1,1 +1,13 @@
 //reducers
+import React from 'react';
+import MainPanel from './components/MainPanel';
+
+function App() {
+  return (
+      <div>
+        <MainPanel/>
+      </div>
+    );
+}
+
+export default App;
